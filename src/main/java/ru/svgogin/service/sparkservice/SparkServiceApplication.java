d7fc59e73b5bc7569ee7f12278a3bdd19fd36ba6
alinkov <aleksanderlinkov@gmail.com>
2020-12-14 22:42:56 +0300

    Init project

---
+++ src/main/java/ru/svgogin/service/sparkservice/SparkServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.svgogin.service.sparkservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/svgogin/service/sparkservice/SparkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.svgogin.service.sparkservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
