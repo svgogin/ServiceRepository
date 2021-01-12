7062e826a5c40cdfe79bb563d9a1c675f062f158
svgogin <243862aks>
2021-01-12 12:20:21 +0300

    #7 Add Repository 1. Repository class renamed 2. Methods toDto/toEntity replaced to SparkService 3. Methods save and update return CompanyDto 4. Repository added 5. Code cleared according to checkstyle

--- src/main/java/ru/svgogin/service/spark/web/SparkController.java
+++ src/main/java/ru/svgogin/service/spark/web/SparkController.java
@@ -1,30 +1,49 @@
 package ru.svgogin.service.spark.web;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.svgogin.service.spark.dto.CompanyDto;
 import ru.svgogin.service.spark.service.SparkService;
-import ru.svgogin.service.spark.entity.Company;
 
 
 @RestController
-@RequestMapping ("/spark")
+@RequestMapping("/spark/companies")
 public class SparkController {
-    private final SparkService sparkService;
+  private final SparkService sparkService;
 
   public SparkController(SparkService sparkService) {
     this.sparkService = sparkService;
-    }
+  }
 
-  @GetMapping("/companies")
-  public Iterable<Company> getCompanies() {
+  @GetMapping()
+  public Iterable<CompanyDto> getCompanies() {
     return sparkService.findAll();
   }
 
-  @GetMapping("/companies/{inn}")
-  public Company getCompanyByInn(@PathVariable("inn") String inn) {
+  @GetMapping("/{inn}")
+  public CompanyDto getCompanyByInn(@PathVariable("inn") String inn) {
     return sparkService.findByInn(inn);
   }
-}
 
+  @PostMapping()
+  public CompanyDto saveCompany(@RequestBody CompanyDto companyDto) {
+    return sparkService.save(companyDto);
+  }
+
+  @PutMapping("/{inn}")
+  public CompanyDto updateCompany(@PathVariable("inn") String inn,
+                                  @RequestBody CompanyDto companyDto) {
+    return sparkService.update(inn, companyDto);
+  }
+
+  @DeleteMapping("/{inn}")
+  public void deleteCompany(@PathVariable("inn") String inn) {
+    sparkService.delete(inn);
+  }
+}
