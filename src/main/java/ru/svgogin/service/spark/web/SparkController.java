515957be7754594273a84d0982270f6cef7f6472
svgogin <243862aks>
2021-02-08 13:25:48 +0300

    #12 Add spring data repository 1. Spring data jdbc repository added 2. Methods to the interface SparkRepositoryDb added (in order to work with Inn instead of id

--- src/main/java/ru/svgogin/service/spark/web/SparkController.java
+++ src/main/java/ru/svgogin/service/spark/web/SparkController.java
@@ -1,5 +1,6 @@
 package ru.svgogin.service.spark.web;
 
+import java.util.Optional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,7 +28,7 @@ public class SparkController {
   }
 
   @GetMapping("/{inn}")
-  public CompanyDto getCompanyByInn(@PathVariable("inn") String inn) {
+  public Optional<CompanyDto> getCompanyByInn(@PathVariable("inn") String inn) {
     return sparkService.findByInn(inn);
   }
 
